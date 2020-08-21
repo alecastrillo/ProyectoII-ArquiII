9ae09e33bf3b07fe2e238fbacb5a54261512e520
Alejandra Castrillo <ale.castrillom97@gmail.com>
2020-08-20 22:00:36 -0600

    Data memory change

--- QuartusProject/dataMemory.sv
+++ QuartusProject/dataMemory.sv
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 module dataMemory(input logic clk, 
 						input logic [15:0] address, 
 						input logic [63:0] inputData, 
@@ -6,22 +5,6 @@ module dataMemory(input logic clk,
 						input logic save,
 						output logic doneSaving,
 						output logic [63:0] out);
-=======
-module dataMemory(clk, address, addressB,
-				  inputData, inputDataB, 
-				  writeEnable, writeEnableB,
-				  out, outB);
-
-	// --- INPUT SIGNALS --- //
-	input clk;
-	input [15:0] address, addressB;
-	input [63:0] inputData, inputDataB;
-	input writeEnable, writeEnableB;
-	
-	// --- OUTPUT SIGNALS --- //
-	output logic [63:0] out, outB;
-	
->>>>>>> 30ec9eb94316bbb60b4bc83dc25cfd4ed91d32e8
 	
 	logic [63:0] mem [15450:0];
 	
@@ -40,15 +23,7 @@ module dataMemory(clk, address, addressB,
 	always_comb begin 
 		if(writeEnable) begin
 			mem[address] <= inputData;
-<<<<<<< HEAD
 	end 
-=======
-			out = inputData;
-		end
-		
-		else out <= mem[address];
-	end
->>>>>>> 30ec9eb94316bbb60b4bc83dc25cfd4ed91d32e8
 	
 	always_ff @(posedge clk) begin
 		if(writeEnableB) begin
@@ -58,39 +33,5 @@ module dataMemory(clk, address, addressB,
 		
 		else outB <= mem[addressB];
 	end
-	
 
-
-<<<<<<< HEAD
 endmodule 
-=======
-endmodule
-
-
-//module dataMemory(input logic clk, 
-//						input logic [15:0] address, 
-//						input logic [63:0] inputData, 
-//						input logic writeEnable, 
-//						output logic [63:0] out);
-//	
-//	logic [63:0] mem [49151:0];
-//	
-//	initial begin
-//		$readmemh("hexImage3.txt", mem);
-//	end
-//	
-//	always_ff @(posedge clk) begin
-//		if(writeEnable) begin
-//			mem[address] <= inputData;
-//			out = inputData;
-//		end
-//		
-//		else out <= mem[address];
-//	
-//	
-//	end
-//
-//
-//
-//endmodule
->>>>>>> 30ec9eb94316bbb60b4bc83dc25cfd4ed91d32e8
